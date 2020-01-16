6606bf2f89425afeb8cf810cd7a05f7fa66efc16
lportinari <lvp.celinski@gmail.com>
2020-01-16 09:55:39 -0300

    Mostrando os dados no template

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 # Chamei as funções index e contato do meu arquivo views
-from .views import index, contato
+from .views import index, contato, produto
 
 # criei minha lista urlpatterns como no arquivo urls do projeto e adicionei os meus paths
 urlpatterns = [
-    path('', index),
-    path('contato', contato),
+    path('', index, name='index'),
+    path('contato', contato, name='contato'),
+    path('produto/<int:pk>', produto, name='produto'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render
 
+from .models import Produto
 # criando as minhas funções e retornando os meus templates
 
 
 def index(request):
-    print(dir(request.user))
-    print(f"User: {request.user}")
-
-    if str(request.user) == 'AnonymousUser':
-        teste = 'Usuário não logado'
-    else:
-        teste = 'Usuário logado'
-
+    produtos = Produto.objects.all()
     context = {
         'curso': 'Programação Web com Django Fromework',
         'outro': 'Django é massa!',
-        'logado': teste,
+        'produtos': produtos,
     }
     return render(request, 'index.html', context)
 
 
 def contato(request):
     return render(request, 'contato.html')
+
+
+def produto(request, pk):
+    print(f'PK: {pk}')
+    return render(request, 'produto.html')
